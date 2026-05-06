7b0555b577cd4dcf01f9a6f218a9e739352a717f
Valentin Batz <hrzlgnm@users.noreply.github.com>
2026-05-06 21:36:01 +0200

    feat: add mdns-tui-browser Cask (#1)

---
+++ Casks/mdns-tui-browser.rb
@@ -0,0 +1,15 @@
+# Copyright 2026 hrzlgnm
+# SPDX-License-Identifier: MIT-0
+
+cask "mdns-tui-browser" do
+  version "1.30.5"
+  sha256 arm:   "761dd5c188e1ff13c53773d09c877a4a38c324179cac28ce7907369b452ff5e1",
+         intel: "0565459c5ef790b7562963e378282f13e077e4c8c2071baaca612b73f9d7b516"
+
+  url "https://github.com/hrzlgnm/mdns-tui-browser/releases/download/v#{version}/mdns-tui-browser-v#{version}-macOS-#{arch == :arm ? "aarch64" : "x86_64"}.dmg"
+  name "mdns-tui-browser"
+  desc "Terminal UI for mDNS service discovery"
+  homepage "https://github.com/hrzlgnm/mdns-tui-browser"
+
+  binary "mdns-tui-browser"
+end
