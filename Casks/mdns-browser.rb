92487ad530f423599451fb387f5f2854ff6f2b2e
hrzlgnm <hrzlgnm@users.noreply.github.com>
2026-08-01 16:04:26 +0000

    Update mdns-browser to v1.12.0

--- Casks/mdns-browser.rb
+++ Casks/mdns-browser.rb
@@ -2,8 +2,8 @@
 # SPDX-License-Identifier: MIT-0
 
 cask "mdns-browser" do
-  version "1.11.1"
-  sha256 "c7d57e2686931f6f84fd45b635f22b2ff3fcf1b09edc630ad2eb858a9e1baddb"
+  version "1.12.0"
+  sha256 "b704de223751c35962e606f941202b878171a068656425ef7f8b2be6700c56ae"
 
   url "https://github.com/hrzlgnm/mdns-browser/releases/download/mdns-browser-v#{version}/mdns-browser_#{version}_universal.dmg"
   name "mDNS Browser"
