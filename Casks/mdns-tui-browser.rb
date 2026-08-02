50b307f620b44868b776cc1ee167288565d65cf6
hrzlgnm <hrzlgnm@users.noreply.github.com>
2026-08-02 14:18:52 +0200

    Revert "chore: update mdns-tui-browser to 1.31.0"
    This reverts commit 6e7ae7b9291e255cd40e7fcf2835c915903b953a.

--- Casks/mdns-tui-browser.rb
+++ Casks/mdns-tui-browser.rb
@@ -2,11 +2,11 @@
 # SPDX-License-Identifier: MIT-0
 
 cask "mdns-tui-browser" do
-  version "1.31.0"
+  version "1.30.21"
   arch arm: "aarch64", intel: "x86_64"
 
-  sha256 arm:   "138f2ec006cd67a17fd73037c969c491dbcfce5764914bcf1aa8a9d5e0987899",
-         intel: "2e62ab4698efd6fa29a2614f76fd73757af7a4e09191aac285e5803c45594e0f"
+  sha256 arm:   "5f8683b0fa3b39101589b6970ee9cdd37ae7a6d22d7e5fe7d4fa260f67c4d2e1",
+         intel: "6e99ed56ee2b339bab602278fcffa228608d5a5847a3660f2d3d7d2afd23cfce"
 
   url "https://github.com/hrzlgnm/mdns-tui-browser/releases/download/v#{version}/mdns-tui-browser-v#{version}-macOS-#{arch}.dmg"
   name "mdns-tui-browser"
