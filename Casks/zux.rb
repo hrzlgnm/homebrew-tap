2b7fb85404f2a1d6e531a93ca796cff69f927acf
Valentin Batz <hrzlgnm@users.noreply.github.com>
2026-08-07 21:58:35 +0200

    chore: correct homepage of zux

--- Casks/zux.rb
+++ Casks/zux.rb
@@ -8,7 +8,7 @@ cask "zux" do
   url "https://github.com/hrzlgnm/zux-homebrew/releases/download/v#{version}/zux_#{version}_universal.dmg"
   name "zux"
   desc "mDNS-SD visualizer with force-directed graph visualization"
-  homepage "https://github.com/hrzlgnm/zux-homebrew"
+  homepage "https://github.com/hrzlgnm/zux"
 
   app "zux.app"
 
