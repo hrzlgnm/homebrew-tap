b945f8c6a6b9096e190bd4c6ba035c6ee5c476c1
Valentin Batz <hrzlgnm@users.noreply.github.com>
2026-08-08 12:02:06 +0200

    chore: use new tag format for download links

--- Casks/mdns-browser.rb
+++ Casks/mdns-browser.rb
@@ -5,7 +5,7 @@ cask "mdns-browser" do
   version "1.15.0"
   sha256 "c901e72a3094220797c3f5d619be2eaf757dc7267d6206c731732ba0f17564a4"
 
-  url "https://github.com/hrzlgnm/mdns-browser/releases/download/mdns-browser-v#{version}/mdns-browser_#{version}_universal.dmg"
+  url "https://github.com/hrzlgnm/mdns-browser/releases/download/v#{version}/mdns-browser_#{version}_universal.dmg"
   name "mDNS Browser"
   desc "Browse mDNS services on your network"
   homepage "https://github.com/hrzlgnm/mdns-browser"
